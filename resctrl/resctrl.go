ddc4e033ff15747415ac0ebcae249fa82731b845
huaqiang <davidhqwang@gmail.com>
2018-04-28 08:04:23 -0400

    Interface for add task to policy group and dump llc occupancy

--- resctrl/resctrl.go
+++ resctrl/resctrl.go
@@ -2,18 +2,20 @@ package resctrl
 
 import (
 	"fmt"
-	"time"
-	"os"
-	"syscall"
 	"io/ioutil"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
+	"syscall"
+	"time"
 )
 
 /*
 #include <stdlib.h>
+#include <stdio.h>
 
 unsigned long initCache(int size){
+	printf("Cache size is %d\n",size);
 	char * pc = (char*)malloc(size);
 	return (unsigned long)pc;
 }
@@ -33,21 +35,21 @@ void cacheLoop(unsigned long pl, int size){
 		*(pc+i) = (char)i;
 	}
 }
- */
+*/
 import "C"
 
-type ResctrlFs struct{
+type ResctrlFs struct {
 	bMount bool
 }
 
-func NewResctrlFs()( *ResctrlFs,  error){
+func NewResctrlFs() (*ResctrlFs, error) {
 	res := &ResctrlFs{
-		bMount:false,
+		bMount: false,
 	}
 	return res, nil
 }
 
-func (r *ResctrlFs) Mount(){
+func (r *ResctrlFs) Mount() {
 	fmt.Println("Mount 'resctrl' file system")
 	r.bMount = true
 }
@@ -59,7 +61,7 @@ func (r *ResctrlFs) Mount(){
 func (r *ResctrlFs) CreateCacheQuota(
 	cpumask []uint64,
 	cpumaskLen int64,
-	cbmMask int64 ){
+	cbmMask int64) {
 	fmt.Println("Create cache allocation policy")
 }
 
@@ -67,25 +69,26 @@ func (r *ResctrlFs) CreateCacheQuota(
 // - Total cache
 // - cache way
 // - capacity per cache way
-func (r * ResctrlFs) CacheInfo(){
+func (r *ResctrlFs) CacheInfo() {
 
 }
 
-func (r* ResctrlFs) WkldAllocaCache(size int) int64 {
+func (r *ResctrlFs) WkldAllocaCache(size int) int64 {
 	return int64(C.initCache(C.int(size)))
 }
 
-func (r* ResctrlFs) WkldFreeCache(pl int64){
+func (r *ResctrlFs) WkldFreeCache(pl int64) {
 	C.deInitCache(C.ulong(pl))
 }
 
-func (r*ResctrlFs) WkldCacheLoop(pl int64, size int){
-	C.cacheLoop(C.ulong(pl), C.int(size) )
+func (r *ResctrlFs) WkldCacheLoop(pl int64, size int) {
+	C.cacheLoop(C.ulong(pl), C.int(size))
 
 }
+
 // Workload for a specific cache occupancy
-func (r* ResctrlFs) WkldCache( occupancyKB int, delay time.Duration) bool{
-	addr := r.WkldAllocaCache(occupancyKB*1024)
+func (r *ResctrlFs) WkldCache(occupancyKB int, delay time.Duration) bool {
+	addr := r.WkldAllocaCache(occupancyKB * 1024)
 	if addr == 0 {
 		return false
 	}
@@ -101,47 +104,55 @@ func (r* ResctrlFs) WkldCache( occupancyKB int, delay time.Duration) bool{
 
 const ResPath = "/sys/fs/resctrl"
 
-func (r*ResctrlFs) CreateMONGroup( group string) bool{
-	folderName := ResPath+"/"+group
-	if stat,err := os.Stat(folderName); err==nil && stat.IsDir(){
+func (r *ResctrlFs) CreateMONGroup(group string) bool {
+	folderName := ResPath + "/" + group
+	if stat, err := os.Stat(folderName); err == nil && stat.IsDir() {
 		return true
 	}
-	if err := os.Mkdir(folderName,0555); err != nil {
+	if err := os.Mkdir(folderName, 0555); err != nil {
 		fmt.Print(err.Error())
 		return false
 	}
 	return true
 }
 
-func (r*ResctrlFs) DestroyMonGroup( group string) bool{
-	folderName := ResPath+"/"+group
-	if stat,err := os.Stat(folderName); err==nil && stat.IsDir() {
+func (r *ResctrlFs) DestroyMonGroup(group string) bool {
+	folderName := ResPath + "/" + group
+	if stat, err := os.Stat(folderName); err == nil && stat.IsDir() {
 		syscall.Rmdir(folderName)
 	}
 
-	if _,err := os.Stat(folderName); os.IsNotExist(err){
+	if _, err := os.Stat(folderName); os.IsNotExist(err) {
 		return true
 	}
 	return false
 }
 
-func (r*ResctrlFs) CheckL3CacheOccupancy(group string) []int64{
-	folderName := ResPath+"/" + group + "/mon_data/"
+func (r *ResctrlFs) CheckL3CacheOccupancy(group string) []int64 {
+	folderName := ResPath + "/" + group + "/mon_data/"
 	var occuL3 []int64
-	if stat,err := os.Stat(folderName); err==nil && stat.IsDir() {
+	if stat, err := os.Stat(folderName); err == nil && stat.IsDir() {
 		const occuL3FolderBase = "mon_L3_0"
 		files, err := ioutil.ReadDir(folderName)
-		if err != nil{
+		if err != nil {
 			return nil
 		}
 		for _, f := range files {
-			if f.IsDir() && strings.Contains(f.Name(), occuL3FolderBase){
-				file,err := os.Open(folderName+"/"+f.Name()); if err==nil{
-					buf := make([]byte,256)
+			if f.IsDir() && strings.Contains(f.Name(), occuL3FolderBase) {
+				os.Chdir(folderName + "/" + f.Name())
+				file, err := os.Open("llc_occupancy")
+				if err == nil {
+					buf := make([]byte, 256)
 					file.Read(buf)
-					occpancy,_ := strconv.ParseInt(string(buf), 10, 0)
-					occuL3 = append(occuL3, occpancy)
-					//fmt.Println(f.Name()," cache occupancy ", occpancy)
+					//fmt.Println(buf)
+					lines := strings.Split(string(buf), "\n")
+					strOccu := "0"
+					if len(lines) != 0 {
+						strOccu = lines[0]
+					}
+					//fmt.Println(strOccu)
+					occupancy, _ := strconv.ParseInt(strOccu, 10, 0)
+					occuL3 = append(occuL3, occupancy)
 				}
 				file.Close()
 			}
@@ -151,46 +162,56 @@ func (r*ResctrlFs) CheckL3CacheOccupancy(group string) []int64{
 	return occuL3
 }
 
-func (r * ResctrlFs) BindTasktoGroup(
+func (r *ResctrlFs) BindTasktoGroup(
 	pid int,
-	group string ) error {
+	group string) error {
 
-	fileName := ResPath + "/" + group + "/tasks"
-	fileCPUList, err := os.OpenFile(fileName,os.O_APPEND|os.O_WRONLY,0); if err!=nil {
+	if err := os.Chdir(ResPath + "/" + group); err != nil {
+		fmt.Println("Error in chdir, ", err.Error())
+	}
+	//dir,_= os.Getwd();fmt.Println(" - ",dir);
+	fileMask, err := os.OpenFile("tasks", os.O_WRONLY, os.ModePerm)
+	if err != nil {
 		fmt.Println("Error in open tasks file: ", err.Error())
 		return err
 	}
-	_,err = fileCPUList.WriteString(string(pid)); if err!=nil{
+	//fmt.Println("pid=",strconv.Itoa(pid))
+	_, err = fileMask.WriteString(strconv.Itoa(pid))
+	if err != nil {
 		fmt.Println("Error in set tasks", err.Error())
 	}
-	fileCPUList.Close()
+	fileMask.Close()
 	return err
 }
 
-func (r* ResctrlFs) BindCPUtoGroup(
-	cpuRangeStart int, cpuRangeEnd int, group string) error{
-	fileName := ResPath + "/" + group + "/cpus_list"
-	fileCPUList, err := os.OpenFile(fileName,os.O_APPEND|os.O_WRONLY,0); if err!=nil {
+func (r *ResctrlFs) BindCPUtoGroup(
+	cpuRangeStart int, cpuRangeEnd int, group string) error {
+	os.Chdir(ResPath + "/" + group)
+	fileCPUList, err := os.OpenFile("cpus_list", os.O_RDWR, os.ModePerm)
+	if err != nil {
 		fmt.Println("Error in open cpus_list file: ", err.Error())
 		return err
 	}
-	_,err = fileCPUList.WriteString(
-		string(cpuRangeStart)+"-"+string(cpuRangeEnd)); if err!=nil{
+	_, err = fileCPUList.WriteString(
+		strconv.Itoa(cpuRangeStart) + "-" + strconv.Itoa(cpuRangeEnd))
+	if err != nil {
 		fmt.Println("Error in append CPU list", err.Error())
 	}
 	fileCPUList.Close()
 	return err
 }
 
-func (r* ResctrlFs) SetCacheSettoGroup(
-	strSchemata string, group string) error{
-	fileName := ResPath + "/" + group + "/schemata"
-	fileCPUList, err := os.OpenFile(fileName,os.O_APPEND|os.O_WRONLY,0); if err!=nil {
+func (r *ResctrlFs) SetCacheSettoGroup(
+	strSchemata string, group string) error {
+	os.Chdir(ResPath + "/" + group)
+	fileCPUList, err := os.OpenFile("schemata", os.O_WRONLY, os.ModePerm)
+	if err != nil {
 		fmt.Println("Error in open schemata file: ", err.Error())
 		return err
 	}
 	// TODO: or get the detail error messgae from last_cmd_status
-	_,err = fileCPUList.WriteString(strSchemata); if err !=nil{
+	_, err = fileCPUList.WriteString(strSchemata)
+	if err != nil {
 		fmt.Println("Error in set schemata", err.Error())
 	}
 	fileCPUList.Close()
--- resctrl/resctrl_test.go
+++ resctrl/resctrl_test.go
@@ -1,19 +1,19 @@
 package resctrl_test
 
 import (
-	"testing"
 	"../resctrl"
-	"os"
-	"time"
 	"fmt"
+	"os"
+	"testing"
+	"time"
 )
 
-func TestWkldResourceAllocate(t* testing.T){
+func TestWkldResourceAllocate(t *testing.T) {
 	t.Log("Step1 - Allocating a specified size cache, size = 1M")
-	r,_ := resctrl.NewResctrlFs()
-	addr := r.WkldAllocaCache(1024*1024)
-	t.Log("   - Got allocated address ",addr)
-	if (addr == 0) {
+	r, _ := resctrl.NewResctrlFs()
+	addr := r.WkldAllocaCache(1024 * 1024)
+	t.Log("   - Got allocated address ", addr)
+	if addr == 0 {
 		t.Error("Nil address returned")
 		return
 	}
@@ -25,16 +25,16 @@ func TestWkldResourceAllocate(t* testing.T){
 	r.WkldCache(1024, time.Second)
 }
 
-func TestMonGroup(t * testing.T){
+func TestMonGroup(t *testing.T) {
 	stage := 0
 	const group = "p1"
-	r,_ := resctrl.NewResctrlFs()
-	if err:=r.CreateMONGroup(group); err{
+	r, _ := resctrl.NewResctrlFs()
+	if err := r.CreateMONGroup(group); err {
 		stage += 1
 		occuL3 := r.CheckL3CacheOccupancy(group)
 		//t.Log("len of occupancy of L3 cache is ", len(occuL3))
-		for i,occu := range occuL3 {
-			t.Log("  L3 ",i," = ",occu)
+		for i, occu := range occuL3 {
+			t.Log("  L3 ", i, " = ", occu)
 		}
 		stage += 1
 		r.DestroyMonGroup(group)
@@ -47,36 +47,36 @@ func TestMonGroup(t * testing.T){
 
 }
 
-func cacheComsumer(occupancyKB int, r * resctrl.ResctrlFs){
-	cacheSize := occupancyKB*1024
+func cacheComsumer(occupancyKB int, r *resctrl.ResctrlFs) {
+	cacheSize := occupancyKB * 1024
 	addr := r.WkldAllocaCache(cacheSize)
-	for ;;{
+	for {
 		r.WkldCacheLoop(addr, cacheSize)
 	}
 	defer r.WkldFreeCache(addr)
 }
 
-func dumpCacheOccupency(group string, r*resctrl.ResctrlFs){
-	for ;; {
+func dumpCacheOccupency(group string, r *resctrl.ResctrlFs) {
+	for {
 		occuL3 := r.CheckL3CacheOccupancy(group)
-		for i,occu := range occuL3 {
-			fmt.Println("  L3 ",i," = ",occu)
+		for i, occu := range occuL3 {
+			fmt.Println("  L3 ", i, " = ", occu)
 		}
 		time.Sleep(time.Second)
 	}
 }
 
-func TestBindTaskToGroup(t * testing.T){
+func TestBindTaskToGroup(t *testing.T) {
 	const group = "p1"
-	r,_ := resctrl.NewResctrlFs()
-	if err:=r.CreateMONGroup(group); err{
+	r, _ := resctrl.NewResctrlFs()
+	if err := r.CreateMONGroup(group); err {
 		pid := os.Getpid()
 		r.BindTasktoGroup(pid, group)
-		r.BindCPUtoGroup(4,8,group)
+		r.BindCPUtoGroup(4, 8, group)
 
 		go cacheComsumer(5*1024, r)
 		go dumpCacheOccupency(group, r)
 	}
-	time.Sleep(5)
+	time.Sleep(50 * time.Second)
 	r.DestroyMonGroup(group)
 }
